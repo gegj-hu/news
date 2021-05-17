742d79e57347d01ade2d35a96f1a643a8212f5ae
gegj-hu <zlp4435558503@163.com>
2021-05-17 10:29:11 +0800

    集成redis

--- manager.py
+++ manager.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from  redis import StrictRedis
+from flask_wtf import CSRFProtect
 
 class Config(object):
     DEBUG = True
@@ -7,7 +9,11 @@ class Config(object):
 
     SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/news"
     SQLALCHEMY_TRACK_MODIFICATIONS = True
+    REDIS_HOST = '127.0.0.1'
+    REDIS_PORT = 6379
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
+redis_store = StrictRedis(host=Config.REDIS_HOST,port=Config.REDIS_PORT)
+
